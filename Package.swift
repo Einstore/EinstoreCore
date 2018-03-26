e9ea453481458a8868f786499bcded8174ef7bec
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-03-26 17:30:17 +0100

    initial upload

---
+++ Package.swift
@@ -0,0 +1,67 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "BoostCore",
+    products: [
+        .library(name: "BoostCore", targets: ["BoostCore"]),
+        .library(name: "BoostCoreTestTools", targets: ["BoostCoreTestTools"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.2"),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: "4.0.2"),
+        .package(url: "https://github.com/LiveUI/ErrorsCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/DbCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/ApiCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/MailCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/SettingsCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/VaporTestTools.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/FluentTestTools.git", .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                "Vapor",
+                "BoostCore"
+            ]
+        ),
+        .target(name: "Run", dependencies: [
+            "App"
+            ]
+        ),
+        .target(name: "BoostCore", dependencies: [
+            "Vapor",
+            "Fluent",
+            "FluentPostgreSQL",
+            "ApiCore",
+            "ErrorsCore",
+            "DbCore",
+            "SwiftShell",
+            "MailCore",
+            "SettingsCore"
+            ]
+        ),
+        .target(
+            name: "BoostCoreTestTools",
+            dependencies: [
+                "Vapor",
+                "ApiCore",
+                "BoostCore",
+                "VaporTestTools",
+                "ApiCoreTestTools",
+                "MailCoreTestTools"
+            ]
+        ),
+        .testTarget(name: "BoostCoreTests", dependencies: [
+            "BoostCore",
+            "VaporTestTools",
+            "FluentTestTools",
+            "ApiCoreTestTools",
+            "BoostCoreTestTools"
+            ]
+        )
+    ]
+)
---
+++ Sources/App/boot.swift
@@ -0,0 +1,11 @@
+import Routing
+import Vapor
+import BoostCore
+
+/// Called after your application has initialized.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#bootswift)
+
+public func boot(_ app: Application) throws {
+    try Boost.boot(app)
+}
