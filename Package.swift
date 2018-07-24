9408020930776c03d67ed93529da57828ac1c382
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-07-24 16:19:31 +0100

    updating package file

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
         .library(name: "BoostCoreTestTools", targets: ["BoostCoreTestTools"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.4"),
         .package(url: "https://github.com/kareman/SwiftShell.git", from: "4.0.2"),
         .package(url: "https://github.com/LiveUI/S3.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/ErrorsCore.git", .branch("master")),
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/LiveUI/ApiCore.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/MailCore.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/SettingsCore.git", .branch("master")),
-        .package(url: "https://github.com/LiveUI/VaporTestTools.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5"),
         .package(url: "https://github.com/LiveUI/FluentTestTools.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/Configure.git", .branch("master"))
     ],
