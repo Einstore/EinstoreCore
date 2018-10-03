a5d09cb911279f7f2d336b99dd271c9ab9b47050
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-10-03 22:52:36 +0100

    can add new tags to app, upgrade of ApiCore

--- Sources/BoostCore/Controllers/TagsController.swift
+++ Sources/BoostCore/Controllers/TagsController.swift
@@ -10,6 +10,7 @@ import Vapor
 import ApiCore
 import Fluent
 import FluentPostgreSQL
+import ErrorsCore
 
 
 class TagsController: Controller {
@@ -19,6 +20,23 @@ class TagsController: Controller {
         router.get("apps", DbIdentifier.parameter, "tags") { (req) -> Future<Tags> in
             let appId = try req.parameters.next(DbIdentifier.self)
             return try TagsManager.tags(appId: appId, on: req)
+        }
+        
+        router.post("apps", DbIdentifier.parameter, "tags") { (req) -> Future<Response> in
+            let appId = try req.parameters.next(DbIdentifier.self)
+            return try [String].fill(post: req).flatMap(to: Response.self) { tags in
+                guard !tags.isEmpty else {
+                    throw ErrorsCore.HTTPError.missingRequestData
+                }
+                return try req.me.teams().flatMap(to: Response.self) { teams in
+                    return try App.query(on: req).safeApp(appId: appId, teamIds: teams.ids).first().flatMap(to: Response.self) { app in
+                        guard let app = app else {
+                            throw ErrorsCore.HTTPError.notFound
+                        }
+                        return try TagsManager.save(tags: tags, for: app, on: req).asResponse(to: req)
+                    }
+                }
+            }
         }
         
         router.get("tags") { (req) -> Future<Tags> in
