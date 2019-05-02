e541e38ed7b9c1b4b67034aa10d6d0bdef04f559
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-05-02 14:45:19 +0100

    sdk gives the latest built

--- Sources/EinstoreCore/Controllers/SdkController.swift
+++ Sources/EinstoreCore/Controllers/SdkController.swift
@@ -15,24 +15,26 @@ import ErrorsCore
 class SdkController: Controller {
     
     static func boot(router: Router, secure: Router, debug: Router) throws {
-        router.post("sdk") { (req) -> EventLoopFuture<SdkInfo> in
+        router.post("sdk") { (req) -> EventLoopFuture<App> in
             guard let token = req.http.headers.authorizationToken else {
                 throw AuthError.authenticationFailed
             }
-            return try ApiKey.query(on: req).filter(\ApiKey.token == token.sha()).filter(\ApiKey.type == 1).first().flatMap(to: SdkInfo.self) { token in
+            return try ApiKey.query(on: req).filter(\ApiKey.token == token.sha()).filter(\ApiKey.type == 1).first().flatMap(to: App.self) { token in
                 guard let token = token else {
                     throw AuthError.authenticationFailed
                 }
                 return try req.content.decode(SdkInfo.self).flatMap({ info in
-                    return Cluster.query(on: req).filter(\Cluster.identifier == info.identifier).filter(\Cluster.platform == info.platform).filter(\Cluster.teamId == token.teamId).first().flatMap(to: SdkInfo.self) { cluster in
+                    return Cluster.query(on: req).filter(\Cluster.identifier == info.identifier).filter(\Cluster.platform == info.platform).filter(\Cluster.teamId == token.teamId).first().flatMap(to: App.self) { cluster in
                         guard let cluster = cluster else {
                             throw ErrorsCore.HTTPError.notFound
                         }
                         
-                        // get latest build from the cluster
-                        
-                        
-                        return req.eventLoop.newSucceededFuture(result: info)
+                        return try cluster.apps.query(on: req).sort(\App.built, .descending).first().map({ app in
+                            guard let app = app else {
+                                throw ErrorsCore.HTTPError.notFound
+                            }
+                            return app
+                        })
                     }
                 })
             }
