9d0e7a07208bbd031e60ae2a8ea57b30b827d77b
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-05-30 09:58:52 +0100

    upgrade tp ApiCore 1.0.0

--- Package.swift
+++ Package.swift
@@ -11,14 +11,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.4"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         .package(url: "https://github.com/kareman/SwiftShell.git", from: "4.0.2"),
-        .package(url: "https://github.com/LiveUI/S3.git", .branch("master")),
-        .package(url: "https://github.com/LiveUI/ErrorsCore.git", .branch("master")),
-        .package(url: "https://github.com/LiveUI/ApiCore.git", .branch("master")),
-        .package(url: "https://github.com/LiveUI/MailCore.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/S3.git", from: "3.0.0"),
+        .package(url: "https://github.com/LiveUI/ErrorsCore.git", from: "0.1.0"),
+        .package(url: "https://github.com/LiveUI/ApiCore.git", from: "1.0.0"),
+        .package(url: "https://github.com/LiveUI/MailCore.git", from: "0.2.3"),
         .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5"),
-        .package(url: "https://github.com/LiveUI/FluentTestTools.git", .branch("master")),
+        .package(url: "https://github.com/LiveUI/FluentTestTools.git", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0")
     ],
     targets: [
